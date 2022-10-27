8a773502e07c6b30017b29a6c0d5b0b6ca772225
Tyler <87284562+Tylerj444@users.noreply.github.com>
2022-10-26 21:03:59 -0600

    Create main.py

---
+++ Stable/Stable-1.01/main.py
@@ -0,0 +1,32 @@
+#interact with terminal to add and divide
+
+
+
+print("Choose the desired mathematical equation, press the letter on your keyboard that matches the options below")
+print("Press A to add two numbers")
+print("Press S to subtract two numbers")
+print("Press M to multiply two numbers")
+print("Press D to divide two numbers")
+print("### EXTREMELY IMPORTANT: LETTERS MUST BE IN CAPITALIZED TO WORK, A FIX FOR THIS WILL BE IMPLEMENTED LATER ###")
+
+while True:
+
+#input to the caculator
+        mathoperation = input(str("Please select the operation: "))
+        number1 = float(input("Enter the first number: "))
+        number2 = float(input("Enter the second number: "))
+
+        if mathoperation == "A":
+                print(number1, "+", number2, "=", number1 + number2)
+
+        elif mathoperation == "S":
+                print(number1, "-", number2, "=", number1 - number2)
+         
+        elif mathoperation == "M":
+                print(number1, "*", number2, "=", number1 * number2)
+    
+        elif mathoperation == "D":
+                print(number1, "/", number2, "=", number1 / number2)
+
+        else:
+            print("Error: Invalid letter selected, the letter that was selected was not one of the options.")
